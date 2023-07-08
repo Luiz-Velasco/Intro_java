3c0ec237278ea5826ce102b90c9a4caaab4b5639
luiz-velasco <luizm_vel@hotmail.com>
2023-07-08 16:24:48 -0300

    Exclui o primeiro codigo java
    Criei mais uma classe de calculo de área modo curto
    
    O metodo main agora chama os dois metodos de calcular que agora passaram a ser static

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,16 +9,32 @@ public class Medidas {
 
     // 3.2 - Metodos e funções
     public static void main(String[] args) {
-        System.out.println("Estou aprendendo Java!!!"); // Digitar sout para ter o codigo completo
+    calcularAreaModoCurto();
+    calcularAreaModoExtenso();
+    }
+
+    public static void calcularAreaModoCurto(){
+        System.out.println("Calculo de área modo curto"); // Digitar sout para ter o codigo completo
 
         // Calculo de área - Exemplo: O tamanho do tapete ou do piso
 
-        int largura = 5;
-        int comprimento = 6;
+        int largura = 7;
+        int comprimento = 8;
         int resultato = largura * comprimento;
 
         System.out.println("A área é de " + resultato);
+    }
 
+    public static void calcularAreaModoExtenso(){
+        // Calculo de área - Exemplo: O tamanho do tapete ou do piso
+        System.out.println("Calculo de área modo extenso");
+        int largura;
+        int comprimento;
+        int resiltado;
 
+        largura = 5;
+        comprimento = 6;
+        resiltado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento +"m a área é de " + resiltado);
     }
 }
