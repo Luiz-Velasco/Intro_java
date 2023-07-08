281aca2a8b85bfffddf1e30569c6e999fbbdc95f
luiz-velasco <luizm_vel@hotmail.com>
2023-07-08 15:45:58 -0300

    Initial commit

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,24 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referencia as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+
+    // 3.2 - Metodos e funções
+    public static void main(String[] args) {
+        System.out.println("Estou aprendendo Java!!!"); // Digitar sout para ter o codigo completo
+
+        // Calculo de área - Exemplo: O tamanho do tapete ou do piso
+
+        int largura = 5;
+        int comprimento = 6;
+        int resultato = largura * comprimento;
+
+        System.out.println("A área é de " + resultato);
+
+
+    }
+}
